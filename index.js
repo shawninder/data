0685d614ebf488a9411d5931b60f211dfaafb929
Shawn Inder <shawninder@gmail.com>
2018-10-11 03:07:09 -0400

    taking the docs literally

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const MongoClient = require('mongodb').MongoClient
-const qs = require('qs')
+// const qs = require('qs')
 
 class Data {
   constructor (conf) {
     const auth = conf.username && conf.password ? `${conf.username}:${conf.password}@` : ''
-    const query = {
-      retryWrites: true
+    // const query = {
+      // retryWrites: true
       // replicaSet: conf.replicaSet
-    }
+    // }
     const cluster = (conf.cluster ? `${conf.cluster}-` : '')
     if (conf.username && conf.password) {
       // query.ssl = true
@@ -15,7 +15,7 @@ class Data {
     }
     this.conf = {
       ...conf,
-      connectionString: `mongodb+srv://${auth}${cluster}${conf.hosts}/${conf.databaseName}?${qs.stringify(query)}`
+      connectionString: `mongodb+srv://${auth}${cluster}.mongodb.net/${conf.databaseName}`
     }
     this.queue = [] // Remembers calls made while offlinet to playback later
     this.db = null // This will be the database handler once created and a not-ready flag until then
